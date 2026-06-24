7707f4fae7db8b2b37e1223729b14f4bd51b07a2
Raja S <rajadom883@gmail.com>
2026-06-24 13:26:22 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo{
     public static void main(String[] args) {
         System.out.println("welcome to learn jenkins");
         System.out.println("Itz kings Kingdom");
+        System.out.println("Itz work");
     }
 }
