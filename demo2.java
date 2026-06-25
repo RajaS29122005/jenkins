a59f4b5990e6654d540f0e94574796bf2ae7e7a0
Raja S <rajadom883@gmail.com>
2026-06-25 12:56:06 +0530

    Create demo2.java

---
+++ demo2.java
@@ -0,0 +1,7 @@
+public class demo{
+    public static void main(String[] args) {
+        System.out.println("welcome to learn jenkins");
+        System.out.println("Itz kings Kingdom");
+        System.out.println("done");
+    }
+}
